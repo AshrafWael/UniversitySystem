d80798294d55a68f83eeb36ba9e95b48434d8f6e
Ashraf Wael <ashrafwael582@gmail.com>
2026-05-18 18:29:14 +0200

    Add Student Controller Contain One End Point

---
+++ UniversitySystem.API/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversitySystem.Core.Features.Students.Queries.Models;
+
+namespace UniversitySystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("/student/getlist")]
+        public async Task<IActionResult> GetStudentList()
+        {
+            var Response = await _mediator.Send(new GetStudentListQuery());
+            return Ok(Response);
+
+        }
+    }
+}
